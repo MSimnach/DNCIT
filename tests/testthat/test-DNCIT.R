2d330c83a2cffd153ae4448d30481b9060414974
MSimnach <marco.simnacher@gmail.com>
2024-03-28 16:15:15 +0100

    tests

--- tests/testthat/test-DNCIT.R
+++ tests/testthat/test-DNCIT.R
@@ -1,5 +1,23 @@
 test_that("DNCIT applicable to univariate Gaussian CI X,Y,Z", {
-  n <- 1000; p <- 1; q <- 1; n_seeds <- 100
+  n <- 100; p <- 1; q <- 1
+  X <- matrix(rnorm(n*p), nrow = n, ncol = p)
+  Y <- matrix(rnorm(n), nrow = n)
+  Z <- matrix(rnorm(n*q), nrow = n, ncol = q)
+
+  expect_true(DNCIT(X, Y, Z)$p < 1)
+})
+
+test_that("DNCIT applicable to multivariate X, univariate Y,Z", {
+  n <- 100; p <- 1; q <- 1
+  X <- matrix(rnorm(n*p), nrow = n, ncol = p)
+  Y <- matrix(rnorm(n), nrow = n)
+  Z <- matrix(rnorm(n*q), nrow = n, ncol = q)
+
+  expect_true(DNCIT(X, Y, Z)$p < 1)
+})
+
+test_that("DNCIT applicable to multivariate X,Z, univariate Y", {
+  n <- 100; p <- 10; q <- 2
   X <- matrix(rnorm(n*p), nrow = n, ncol = p)
   Y <- matrix(rnorm(n), nrow = n)
   Z <- matrix(rnorm(n*q), nrow = n, ncol = q)
