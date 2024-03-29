b2f96f8b2a5a37f5d417ca7e03cb410e3da10eda
MSimnach <marco.simnacher@gmail.com>
2024-03-29 13:46:07 +0100

    install virtualenv for python and corresponding readme

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,13 @@ CMIknn <- NULL
 np <- NULL
 
 .onLoad <- function(libname, pkgname) {
-  reticulate::use_virtualenv("r-cits")
+  if(!reticulate::virtualenv_exists('r-cits')){
+    reticulate::py_install(packages = "tigramite", envname = 'r-cits')
+    reticulate::py_install(packages = "scikit_learn", envname = 'r-cits')
+    #reticulate::py_install(packages = "fcit", envname = 'r-cits')
+  }
+  reticulate::use_virtualenv("r-cits", required = FALSE)
+
   # use superassignment to update global reference to scipy
   CMIknn <<- reticulate::import("tigramite.independence_tests.cmiknn", delay_load = TRUE)
   np <<- reticulate::import('numpy', delay_load = TRUE)
