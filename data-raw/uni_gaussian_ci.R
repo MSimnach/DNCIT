d88b4f30e50b54a47bbcc5f2a5bbce6481d114a2
MSimnach <marco.simnacher@gmail.com>
2024-04-02 14:01:15 +0200

    data
    data

--- data-raw/uni_gaussian_ci.R
+++ data-raw/uni_gaussian_ci.R
@@ -4,4 +4,5 @@ n <- 1000; p <- 1; q <- 1
 X <- matrix(rnorm(n*p), nrow = n, ncol = p)
 Y <- matrix(rnorm(n), nrow = n)
 Z <- matrix(rnorm(n*q), nrow = n, ncol = q)
-usethis::use_data(cbind(X, Y, Z) , overwrite = TRUE)
+uni_gaussian_ci <- cbind(X, Y, Z)
+usethis::use_data(uni_gaussian_ci, overwrite = TRUE)
