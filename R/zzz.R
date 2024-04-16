07ebf9a3141f5f6092310c38d9014770fdc28e1d
MSimnach <marco.simnacher@gmail.com>
2024-04-16 11:03:20 +0200

    install also for virtual env manually due to dependencies in fcit wrong

--- R/zzz.R
+++ R/zzz.R
@@ -8,17 +8,21 @@ PIL_image <- NULL
 py_torch <- NULL
 
 .onLoad <- function(libname, pkgname) {
-  py_packages <- c("tigramite", "scikit_learn", "fcit", "open_clip_torch")
+  # py_packages <- c("tigramite", "scikit_learn", "fcit", "open_clip_torch")
 
   if(!(reticulate::virtualenv_exists('r-cits') || reticulate::condaenv_exists('r-cits'))){
     tryCatch({
-      reticulate::virtualenv_create(envname="r-cits", version="<=3.10", packages = py_packages)
+      reticulate::virtualenv_create(envname="r-cits", version="<=3.10")
+      reticulate::py_install(packages = "tigramite", envname = 'r-cits', pip=TRUE)
+      reticulate::py_install(packages = "scikit_learn", envname = 'r-cits')
+      reticulate::py_install(packages = "fcit", envname = 'r-cits',pip=TRUE, pip_options=c("--no-deps"))
+      reticulate::py_install(packages = "open_clip_torch", envname = 'r-cits',pip=TRUE)
     }, error = function(e) {
       # If virtualenv fails, attempt conda
       cat("virtualenv-create failed. Attempting conda_install...\n")
       reticulate::conda_create(envname="r-cits", version="<=3.10")
       reticulate::conda_install(packages = "tigramite", envname = 'r-cits', pip=TRUE)
-      reticulate::conda_install(packages = "scikit_learn", envname = 'r-cits')
+      reticulate::conda_install(packages = "scikit_learn", envname = 'r-cits', pip=TRUE)
       reticulate::conda_install(packages = "fcit", envname = 'r-cits',pip=TRUE, pip_options=c("--no-deps"))
       reticulate::conda_install(packages = "open_clip_torch", envname = 'r-cits',pip=TRUE)
     })
