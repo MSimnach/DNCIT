69ad7b80703101208956ec851c4b7ac4163874c1
MSimnach <marco.simnacher@gmail.com>
2024-04-16 10:37:56 +0200

    install with conda env

--- R/zzz.R
+++ R/zzz.R
@@ -8,21 +8,17 @@ PIL_image <- NULL
 py_torch <- NULL
 
 .onLoad <- function(libname, pkgname) {
-  if(!reticulate::virtualenv_exists('r-cits')){
-    if(!reticulate::py_module_available('virtualenv')){
-      reticulate::py_install('virtualenv', envname='r-cits', pip=TRUE, python_version = "<=3.10")
+  if(!reticulate::condaenv_exists('r-cits')){
+    if(!reticulate::virtualenv_exists('r-cits')){
+      reticulate::virtualenv_create(envname="r-cits", version="<=3.10")
+      reticulate::py_install(packages = "tigramite", envname = 'r-cits', pip=TRUE)
+      reticulate::py_install(packages = "scikit_learn", envname = 'r-cits')
+      reticulate::py_install(packages = "fcit", envname = 'r-cits',pip=TRUE, pip_options=c("--no-deps"))
+      reticulate::py_install(packages = "open_clip_torch", envname = 'r-cits',pip=TRUE)
+      startup::restart_r(quiet=TRUE)
     }
-    reticulate::virtualenv_create(envname="r-cits", version="<=3.10")
-    reticulate::py_install(packages = "tigramite", envname = 'r-cits', pip=TRUE)
-    reticulate::py_install(packages = "scikit_learn", envname = 'r-cits')
-    reticulate::py_install(packages = "fcit", envname = 'r-cits',pip=TRUE, pip_options=c("--no-deps"))
-    reticulate::py_install(packages = "open_clip_torch", envname = 'r-cits',pip=TRUE)
-    if(requireNamespace('startup', quietly = TRUE)){
-      install.packages('startup')
-    }
-    startup::restart_r(quiet=TRUE)
   }
-  reticulate::use_virtualenv("r-cits", required = FALSE)
+  reticulate::use_condaenv("r-cits", required = FALSE)
 
   # use superassignment to update global reference to python modules/packages
   CMIknn <<- reticulate::import("tigramite.independence_tests.cmiknn", delay_load = TRUE)
