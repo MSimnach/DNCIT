20dad43d91c30c284bfd5f7ecaa2ecb24bd37c72
MSimnach <marco.simnacher@gmail.com>
2024-09-30 11:21:49 +0200

    switch prediction y from z with y from x,z in ordering

--- R/pred_cit.R
+++ R/pred_cit.R
@@ -20,16 +20,48 @@
 pred_cit <- function(X,Y,Z, term_time = round(exp(4)/3)){
   # termination time for auto tuner
   term_time_at <- term_time
+  tuner <- mlr3tuning::tnr("random_search")
 
-  ## learn y from x,z
-  yxz <- do.call(cbind, list(Y,X,Z))
-  colnames(yxz) <- paste0("V", 1:ncol(yxz))
-
-  tsk_yxz <-  mlr3::as_task_regr(yxz, target = "V1")
+  ## learn y from z
+  yz <- do.call(cbind, list(Y,Z))
+  colnames(yz) <- paste0("V", 1:ncol(yz))
   measure <- mlr3::msr("regr.mse")
+  learner <- mlr3tuningspaces::lts(mlr3::lrn("regr.ranger", verbose = FALSE))
+
+  tsk_yz <-  mlr3::as_task_regr(yz, target = "V1")
+  #same folds as for y on xz
   rsmp_inner <- mlr3::rsmp("cv", folds = 5)
   rsmp_outer <- mlr3::rsmp("cv", folds = 3)
-  learner <- mlr3tuningspaces::lts(mlr3::lrn("regr.ranger", verbose = FALSE))
+
+  at_yz <- mlr3tuning::auto_tuner(
+    tuner = tuner,
+    learner = learner,
+    resampling = rsmp_inner,
+    measure = measure,
+    term_time = term_time_at,
+    store_models = TRUE
+  )
+
+  rr_yz <- mlr3::resample(tsk_yz, at_yz, rsmp_outer, store_models = TRUE)
+
+  ## learn y_hat from x,z
+  yxz <- do.call(cbind, list(Y,X,Z))
+  colnames(yxz) <- paste0("V", 1:ncol(yxz))
+  tsk_yxz <-  mlr3::as_task_regr(yxz, target = "V1")
+
+  ## same outer resampling splits as for y on z
+  rsmp_custom = mlr3::rsmp("custom")
+  rr_resampling_yxz <- rr_yz$resampling$instance
+  test_splits <- list()
+  train_splits <- list()
+  for (i in 1:rsmp_outer$iters) {
+    test_splits[[i]] <- rr_resampling_yxz[rr_resampling_yxz$"fold"==i,]$row_id
+    train_splits[[i]] <- rr_resampling_yxz[rr_resampling_yxz$"fold"!=i,]$row_id
+  }
+  rsmp_custom$instantiate(tsk_yxz,
+                          train = train_splits,
+                          test = test_splits
+  )
 
   at_yxz <- mlr3tuning::auto_tuner(
     tuner = mlr3tuning::tnr("random_search"),
@@ -39,36 +71,8 @@ pred_cit <- function(X,Y,Z, term_time = round(exp(4)/3)){
     term_time = term_time_at
   )
 
-  rr_yxz <- mlr3::resample(tsk_yxz, at_yxz, rsmp_outer)
+  rr_yxz <- mlr3::resample(tsk_yxz, at_yxz, rsmp_custom)
 
-  ## learn y from z
-  yz <- do.call(cbind, list(Y,Z))
-  colnames(yz) <- paste0("V", 1:ncol(yz))
-
-  tsk_yz <-  mlr3::as_task_regr(yz, target = "V1")
-  #same folds as for y on xz
-  rsmp_custom = mlr3::rsmp("custom")
-  rr_resampling_yxz <- rr_yxz$resampling$instance
-  test_splits <- list()
-  train_splits <- list()
-  for (i in 1:rsmp_outer$iters) {
-    test_splits[[i]] <- rr_resampling_yxz[rr_resampling_yxz$"fold"==i,]$row_id
-    train_splits[[i]] <- rr_resampling_yxz[rr_resampling_yxz$"fold"!=i,]$row_id
-  }
-  rsmp_custom$instantiate(tsk_yz,
-                          train = train_splits,
-                          test = test_splits
-  )
-
-  at_yz <- mlr3tuning::auto_tuner(
-    tuner = mlr3tuning::tnr("random_search"),
-    learner = learner,
-    resampling = rsmp_inner,
-    measure = measure,
-    term_time = term_time_at
-  )
-
-  rr_yz <- mlr3::resample(tsk_yz, at_yz, rsmp_custom)
 
   ## residuals from predictions
   preds_yxz <- rr_yxz$prediction()
